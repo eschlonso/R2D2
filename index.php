b181094d90f9f702bcd5aaa41d35bb6296bb6828
IPGLA\marcos.brandolin <marcosbrandolin@gmail.com>
2016-11-28 00:01:19 -0300

    mejoras diseño responsivo.

--- index.php
+++ index.php
@@ -620,7 +620,10 @@ body {
 	color: #ffffff;
 }
 
-
+.texto2{
+	
+	font-size: 16px;
+}
 <?php if($_GET['b']=="0" or $_GET['b']=="3"){?>
 #tiempo_trakeado43{ display:none;}
 <?php }else{?>
@@ -900,7 +903,7 @@ input{
 
 
 
-	  <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4" style="height:400px;">
+	  <div class="col-xs-12 col-sm-12 col-md-4 col-lg-3" style="height:400px;">
 
 	  <div style="font-size:22px;font-weight:bold;" class="text-center texto1">R2D2 "ARTURITO"</div>
 	  
@@ -910,20 +913,85 @@ input{
 <p class="text-center texto1">es mi ayudante y tanque de ideas</p>
 
 </div>
-<div class="col-xs-12 col-sm-12 col-md-8 col-lg-7 text-center" style="height:100%;">
 
 
 
-	
-	
-			
+
+
+
+<table>
+ <tr>
+    <td>
+
+
+
+
+
+<div class="col-xs-12 col-sm-12 col-md-8 col-lg-12 text-center" style="height:100%;">
+
+
+
+
+
+
+
+
+
+	<div class="col-xs-12 col-sm-12 col-md-6 col-lg-4 text-center">
+		
 			<div align="center" style=" margin-bottom:10px;" id="Fecha_Reloj" class="texto1">
               <div ></div>
             </div>
-         
-			  
+
+
+	</div>
+	<div class="col-xs-12 col-sm-12 col-md-6 col-lg-4 text-center texto1 texto2">
+		
+Temperatura 23 c <br>		
+prob de precipitaciones 0%<br>
+humedad 73%<br>
+viento: a 14 km x hora<br>
+
+
+
+	</div>
+		<div class="col-xs-12 col-sm-12 col-md-6 col-lg-4 text-center texto1 texto2">
+		
+estado transito:
+<br>	tren:<br>calles:	<br>subtes:<br>	
+
+
+	</div>
 			
-			  
+		
+         
+	
+
+
+
+
+
+
+
+
+
+
+	</div>	
+
+
+
+    </td>
+  </tr>
+   <tr>
+    <td>
+    	
+
+
+<div class="col-xs-6 col-sm-4 col-md-3 col-lg-12">
+
+
+
+		  
 	<div class="col-xs-6 col-sm-4 col-md-3 col-lg-3 text-center botonsitos" style="">
 		
 		    <input type="submit" name="Submit37" value="Finanzas" class="mibotonsito"  style="background:white url('FINANZAS.jpg');background-size: 65px 65px;background-repeat:no-repeat; background-position:50% 0px; " onClick="window.open('finanzas/index.php','_blank')">
@@ -1041,7 +1109,7 @@ input{
 
 	</div>	
 
-		<div class="col-xs-6 col-sm-4 col-md-3 col-lg-3 text-centerbotonsitos" style="">
+		<div class="col-xs-6 col-sm-4 col-md-3 col-lg-3 text-center botonsitos" style="">
 		
 
 
@@ -1051,7 +1119,7 @@ input{
 
 	</div>	
 
-		<div class="col-xs-6 col-sm-4 col-md-3 col-lg-3 text-centerbotonsitos" style="">
+		<div class="col-xs-6 col-sm-4 col-md-3 col-lg-3 text-center botonsitos" style="">
 		
 
                           <input type="submit" name="Submit310" value="Notas" class="mibotonsito"  style="background:white url('excel.jpg');background-size: 50px 50px;background-repeat:no-repeat; background-position:50% 7px; " onClick="window.open('https://docs.google.com/spreadsheet/ccc?key=0AuG3DD76WSZBdHUxSVNBaG1KdzFMT2Z6ZGE4NGt5THc&usp=sharing','_blank')">
@@ -1060,10 +1128,16 @@ input{
 
 
 	</div>	
-			  
-			
 
-	</div>	
+</div>
+
+
+    </td>
+  </tr>
+</table>
+
+
+
 
 
 
